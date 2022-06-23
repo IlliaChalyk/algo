a37b5fc52dfa55408b72cd4642570626b27362a1
Illya Chalyk <chalyk.ilya@gmail.com>
2022-06-23 12:56:08 +0300

    make all decode tests pass

--- src/main/java/com/other/RunLengthEncoder.java
+++ src/main/java/com/other/RunLengthEncoder.java
@@ -27,7 +27,33 @@ public class RunLengthEncoder {
         return sb.toString();
     }
 
+    /**
+     * This method can decode only even-length strings with [0, 10)
+     * repeats of single character, otherwise the result is uncertain.
+     */
     public String decode(String input) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if(input == null || input.length() < 1 || input.length() % 2 != 0) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.length(); i +=2) {
+            int times = Integer.parseInt("" + input.charAt(i));
+            char ch = input.charAt(i + 1);
+            StringBuilder segment = repeat(ch, times);
+            sb.append(segment);
+        }
+
+        return sb.toString();
+    }
+
+    private StringBuilder repeat(char ch, int times) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < times; i++) {
+            sb.append(ch);
+        }
+
+        return sb;
     }
 }
