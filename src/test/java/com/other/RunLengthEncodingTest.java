00c78c77a9c621cff7e0547cf0e13ae62a93698c
Illya Chalyk <chalyk.ilya@gmail.com>
2022-06-23 12:06:26 +0300

    add tests for decode method

--- src/test/java/com/other/RunLengthEncodingTest.java
+++ src/test/java/com/other/RunLengthEncodingTest.java
@@ -66,4 +66,84 @@ class RunLengthEncodingTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void shouldDecodeNullString() {
+        String input = null;
+        String expected = "";
+
+        String actual = uut.decode(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDecodeStringOfSameRepetitiveCharacter() {
+        String input = "5f";
+        String expected = "fffff";
+
+        String actual = uut.decode(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDecodeStringOfOneCharacter() {
+        String input = "1g";
+        String expected = "g";
+
+        String actual = uut.decode(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDecodeAnEmptyString() {
+        String input = "";
+        String expected = "";
+
+        String actual = uut.decode(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDecodeMixedCharactersString() {
+        String input = "3j1r2t2n";
+        String expected = "jjjrttnn";
+
+        String actual = uut.decode(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDecodeStringWithSameCharacterOnBothSides() {
+        String input = "1o3h1g1o";
+        String expected = "ohhhgo";
+
+        String actual = uut.decode(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDecodeStringWithDigits() {
+        String input = "11223518";
+        String expected = "1225558";
+
+        String actual = uut.decode(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnEmptyStringWhenDecodeOddLengthString() {
+        String input = "1d3g1";
+        String expected = "";
+
+        String actual = uut.decode(input);
+
+        assertEquals(expected, actual);
+    }
 }
